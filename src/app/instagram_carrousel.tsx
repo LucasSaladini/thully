3cb788eca435a88e1c8b36580434eca934df7ac9
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2025-02-08 22:17:44 -0300

    Atualizar o instagram_carrousel.tsx

--- src/app/instagram_carrousel.tsx
+++ src/app/instagram_carrousel.tsx
@@ -15,10 +15,6 @@ const instagramPhotos = [
     url: "https://instagram.fcpq5-1.fna.fbcdn.net/v/t51.29350-15/464998584_1084069410042986_1038978047577685008_n.heic?stp=dst-jpg_e35_tt6&_nc_ht=instagram.fcpq5-1.fna.fbcdn.net&_nc_cat=102&_nc_oc=Q6cZ2AFkYHM07R9plKoubYx0nmmJK-WGA4JZQDr-InizJPL_1gQ0Iw8KJyT0NFo-XSVwNUK_DBsSBvNrm2u-rV564j2j&_nc_ohc=tWXk8jXlwmYQ7kNvgF9ZQuB&_nc_gid=4deb42c773674e3d915be1eef8f6ae79&edm=ANTKIIoBAAAA&ccb=7-5&oh=00_AYD6Rd9U0AKbSCfljbFKiv8zrJNPDXc2ODOVwa-DLaYUpg&oe=67AD24F1&_nc_sid=d885a2"
   },
   {
-    id: "2",
-    url: "https://instagram.fcpq5-1.fna.fbcdn.net/v/t51.29350-15/464398905_8895774583801044_7704684739389251730_n.heic?stp=dst-jpg_e35_tt6&_nc_ht=instagram.fcpq5-1.fna.fbcdn.net&_nc_cat=100&_nc_oc=Q6cZ2AFRRI4AzGJZf9WTwZkpKIWuvt0eUDrcVFay2stfUsPSylINqI-oXE-FJ6HI4nUuLuBbHkykAvvQa1HcB2JRuTGN&_nc_ohc=IJywjlsgAkoQ7kNvgHotAcd&_nc_gid=04e6bd080a8a41c395e6da382bb2dfc1&edm=ANTKIIoBAAAA&ccb=7-5&oh=00_AYCN-ZptTrFUOME5F3icQKZJXToKggL66eXjz5tUPHt_IA&oe=67AD1C58&_nc_sid=d885a2"
-  },
-  {
     id: "3",
     url: "https://instagram.fcpq5-1.fna.fbcdn.net/v/t51.29350-15/464373105_1066935794887356_6959745604363849456_n.heic?stp=dst-jpg_e35_tt6&_nc_ht=instagram.fcpq5-1.fna.fbcdn.net&_nc_cat=100&_nc_oc=Q6cZ2AG7_72NJ0oANpMMXknRxuJ8y0w-4f1bMap6Mgj2km0sCTXLPbC8rYs2ye-uQyZKsHSUWB2NI6isKlZ75oOuyaM1&_nc_ohc=V9HVs_1aHQMQ7kNvgH44G9I&_nc_gid=d5f3d57374194c7fbe3d61b647d4c4a6&edm=ANTKIIoBAAAA&ccb=7-5&oh=00_AYBsu14m01QBvWYgtNq0CWs5VahObL0TS0flMu5PMFHJ8g&oe=67AD2DD5&_nc_sid=d885a2"
   },
